c025ee80307aeded29f0b376bb5949abe2dcd478
Lance Larsen <lance.c.larsen@gmail.com>
2016-09-26 19:38:40 -0600

    __init__.py: - Add Ssc to list of exported classes.

--- PyPE/__init__.py
+++ PyPE/__init__.py
@@ -1,4 +1,5 @@
-from .PyPE import P, R, S, V, C, Cb, Cc, Cg, Cl, Cp, Col, SOL, EOL, Sc, Sp, Sm
+from .PyPE import P, R, S, V, C, Cb, Cc, Cg, Cl, Cp, Col, SOL, EOL, Sc, Sp, \
+                  Sm, Ssz
 from .PyPE import match, matchUntil, escapeStr, join, whitespace, whitespace0, \
                   whitespace1, alpha, digit, newline, quote, setVs
 from .Tokenizer import Tokenizer
